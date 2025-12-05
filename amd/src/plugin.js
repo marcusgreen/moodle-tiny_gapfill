88be174201edc0620af4bb16e500108227f27fbc
Marcus Green <marcusavgreen@gmail.com>
2025-12-05 15:04:28 +0000

    Fix to plugin.js so button now appears (everywhere)

--- amd/src/plugin.js
+++ amd/src/plugin.js
@@ -1,4 +1,4 @@
-// This file is part of Moodle - http://moodle.org/
+// This file is part of Moodle - https://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -11,45 +11,45 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
 
 /**
- * Tiny gapfill for Moodle
+ * Tiny tiny_gapfill for Moodle.
  *
- * @module     tiny_gapfill/plugin
- * @copyright  2025 2024 Marcus Green
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @module      tiny_gapfill/plugin
+ * @copyright   2023 MoodleDACH
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 import {getTinyMCE} from 'editor_tiny/loader';
 import {getPluginMetadata} from 'editor_tiny/utils';
 
 import {component, pluginName} from './common';
-import * as Options from 'tiny_gapfill/options';
-import * as Commands from 'tiny_gapfill/commands';
+import {getSetup as getCommandSetup} from './commands';
 import * as Configuration from './configuration';
+import {register as registerOptions} from './options';
 
-// Export a Promise that resolves to [pluginName, Configuration].
-// This matches the TinyMCE/Moodle pattern used by other plugins.
-export default new Promise(async (resolve) => {
+// Setup the tiny_gapfill Plugin.
+// eslint-disable-next-line no-async-promise-executor
+export default new Promise(async(resolve) => {
     // Note: The PluginManager.add function does not support asynchronous configuration.
     // Perform any asynchronous configuration here, and then call the PluginManager.add function.
     const [
         tinyMCE,
-        setupCommands,
         pluginMetadata,
+        setupCommands,
     ] = await Promise.all([
         getTinyMCE(),
-        Commands.getSetup(),
         getPluginMetadata(component, pluginName),
+        getCommandSetup(),
     ]);
 
     // Reminder: Any asynchronous code must be run before this point.
     tinyMCE.PluginManager.add(pluginName, (editor) => {
         // Register options.
-        Options.register(editor);
+        registerOptions(editor);
 
-        // Setup the Commands (buttons, menu items, and so on).
+        // Setup any commands such as buttons, menu items, and so on.
         setupCommands(editor);
 
         // Return the pluginMetadata object. This is used by TinyMCE to display a help link for your plugin.
--- classes/plugininfo.php
+++ classes/plugininfo.php
@@ -1,19 +1,4 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
 namespace tiny_gapfill;
 
 use context;
@@ -21,52 +6,66 @@ use editor_tiny\editor;
 use editor_tiny\plugin;
 use editor_tiny\plugin_with_buttons;
 use editor_tiny\plugin_with_menuitems;
-use editor_tiny\plugin_with_configuration;
 
-/**
- * Tiny gapfill plugin for Moodle
- *
- * Documentation: {@link https://moodledev.io/docs/apis/plugintypes/tiny}
- *
- * @package    tiny_gapfill
- * @copyright  2025 2024 Marcus Green
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-class plugininfo extends plugin implements plugin_with_configuration, plugin_with_buttons, plugin_with_menuitems {
+class plugininfo extends plugin implements plugin_with_buttons, plugin_with_menuitems {
 
-    /**
-     * Toolbar buttons provided by this plugin.
-     *
-     * @return array
-     */
+    public static function is_enabled(
+        context $context,
+        array $options,
+        array $fpoptions,
+        ?editor $editor = null
+    ): bool {
+        // Only enable for question editing pages
+        if (!isset($options['context'])) {
+            return false;
+        }
+
+        // Check if we're in a question context
+        $context = $options['context'];
+        if ($context->contextlevel !== CONTEXT_MODULE &&
+            $context->contextlevel !== CONTEXT_COURSE) {
+            return false;
+        }
+
+        // Check if the element ID indicates this is the question text field
+        // The element ID for question text is usually 'id_questiontext'
+        if (isset($options['elementid'])) {
+            $elementid = $options['elementid'];
+            // Only show in the questiontext field
+            if (strpos($elementid, 'questiontext') === false) {
+                return false;
+            }
+        }
+
+        // Check if we're editing a gapfill question type
+        // This checks the URL parameters
+        $qtype = optional_param('qtype', '', PARAM_ALPHA);
+
+        // Also check for existing question being edited
+        if (empty($qtype)) {
+            $questionid = optional_param('id', 0, PARAM_INT);
+            if ($questionid) {
+                global $DB;
+                $question = $DB->get_record('question', ['id' => $questionid], 'qtype');
+                if ($question) {
+                    $qtype = $question->qtype;
+                }
+            }
+        }
+
+        // Only enable for gapfill question type
+        return ($qtype === 'gapfill');
+    }
+
     public static function get_available_buttons(): array {
         return [
             'tiny_gapfill/gapfill',
         ];
     }
 
-    /**
-     * Menu items provided by this plugin.
-     */
     public static function get_available_menuitems(): array {
         return [
             'tiny_gapfill/gapfill',
-        ];
-    }
-
-    /**
-     * Get a list of the menu items provided by this plugin.
-     *
-     * @param context $context The context that the editor is used within
-     * @param array $options The options passed in when requesting the editor
-     * @param array $fpoptions The filepicker options passed in when requesting the editor
-     * @param editor|null $editor The editor instance in which the plugin is initialised
-     * @return array
-     */
-    public static function get_plugin_configuration_for_context(context $context, array $options, array $fpoptions,
-        ?editor $editor = null): array {
-        return [
-            'contextid' => $context->id,
         ];
     }
 }
